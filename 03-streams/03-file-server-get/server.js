c3f2868060f22aca4e99b51b128dec2625492a1a
Glomberg <bazz@bk.ru>
2022-03-26 21:25:14 +0300

    A solution for 03-streams/03-file-server-get added.

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -12,7 +13,17 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'GET':
-
+      const outputStatusCode = res.statusCode = getStatusCode(pathname, filepath);
+      if ( outputStatusCode === 200 ) {
+        res.setHeader("Content-Type", "application/octet-stream");
+        const readStream = fs.createReadStream(filepath);
+        readStream.pipe(res);
+        res.on('close', () => {
+          readStream.destroy();
+        });
+      } else {
+        res.end('Internal programmer error O_o');
+      }
       break;
 
     default:
@@ -20,5 +31,20 @@ server.on('request', (req, res) => {
       res.end('Not implemented');
   }
 });
+
+function getStatusCode(pathname, filepath) {
+  let statusCode = 500;
+  if ( pathname.indexOf('/') !== -1 ) {
+    statusCode = 400;
+  }
+  if ( pathname.indexOf('.') !== -1 ) {
+    if ( fs.existsSync(filepath) ) {
+      statusCode = 200;
+    } else {
+      statusCode = 404;
+    }
+  }
+  return statusCode;
+}
 
 module.exports = server;
