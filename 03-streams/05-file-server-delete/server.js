6462ddd8e037bcf5d94867f0f2823e7a81abd33a
Glomberg <bazz@bk.ru>
2022-04-01 21:32:36 +0300

    A solution for 03-streams/05-file-server-delete added.

--- 03-streams/05-file-server-delete/server.js
+++ 03-streams/05-file-server-delete/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -10,9 +11,28 @@ server.on('request', (req, res) => {
 
   const filepath = path.join(__dirname, 'files', pathname);
 
+  if ( pathname.indexOf('/') !== -1 ) {
+    res.statusCode = 400;
+    res.end('Nesting is not allowed');
+    return;
+  }
+
+  if (! fs.existsSync(filepath)) {
+    res.statusCode = 404;
+    res.end('File is not exist');
+    return;
+  }
+
   switch (req.method) {
     case 'DELETE':
-
+      fs.rm(filepath, (err) => {
+        if (err) {
+          res.statusCode = 500;
+          res.end('Filesystem error: ' + err.toString());
+        }
+        res.statusCode = 200;
+        res.end('OK');
+      });
       break;
 
     default:
